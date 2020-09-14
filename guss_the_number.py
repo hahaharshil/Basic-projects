cc828f38eb397961209c132c5c2d3d5c7b8e8936
hahaharshil <hahaharshil3006@gmail.com>
2020-09-14 16:03:42 +0530

    Sep 14 2020

--- guss_the_number.py
+++ guss_the_number.py
@@ -1,25 +1,45 @@
-import random , sys
+import random
 
-print("Think a number between 1 and 20")
-snumber = random.randint(1 , 20)
-numberoftry = 6
+snum = random.randint(1 , 20)
+
+print("""
+You have to guess the number between 1 and 20.
+You will get chances randomly.
+▐▓█▀▀▀▀▀▀▀▀▀█▓▌░▄▄▄▄▄░
+▐▓█░░▀░░▀▄░░█▓▌░█▄▄▄█░
+▐▓█░░▄░░▄▀░░█▓▌░█▄▄▄█░
+▐▓█▄▄▄▄▄▄▄▄▄█▓▌░█████░
+░░░░▄▄███▄▄░░░░░█████░
+
+""")
+
+
+
+n = random.randint(4 , 10)
+print(f"You got {n} chances")
 
 while True:
-    n = input("Number: ")
-
-    if snumber < int(n) and numberoftry != 0:
-        print("Not that big")
-        numberoftry = numberoftry - 1
-        print(f"{numberoftry} of tries left...")
-
-
-    if snumber > int(n) and numberoftry != 0:
-        print('Think big')
-        numberoftry = numberoftry - 1
-        print(f"{numberoftry} tries left...")
-
-
-    if snumber == int(n) and numberoftry != 0:
+    m = input("Number: ")
+    if int(m) == snum:
         print("You won")
-        print(f"You took {numberoftry} tries")
+        break
+
+    if int(m) < snum:
+        n = n - 1
+        print("Think big")
+        if n > 1:
+            print(f"You have {n} tries left")
+        if n == 1:
+            print(f"You have 1 try left")
+
+    if int(m) > snum:
+        n = n - 1
+        print("Not that big")
+        if n > 1:
+            print(f"You have {n} tries left")
+        if n == 1:
+            print(f"You have 1 try left")
+
+    if n == 0:
+        print('You lost')
         break
